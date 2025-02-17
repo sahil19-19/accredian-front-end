249e8efbe312b8d763d93b6ae8046383a4947a1a
Sahil <sahilverma1931@gmail.com>
2025-02-17 19:22:37 +0530

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from './pages/home/home'
+import Refer from './pages/refer/refer'
+import About from "./pages/about/about";
+// import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      Navbar
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>}></Route>
+                <Route path="/refer" element={<Refer/>}></Route>
+                <Route path="/about" element={<About/>}></Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;

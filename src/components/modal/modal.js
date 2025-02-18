1bb40e99d453f6efc105e02430e10b8e0c3d24ee
Sahil <sahilverma1931@gmail.com>
2025-02-19 01:15:08 +0530

    add snackbar to display status of referrel

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,45 +1,138 @@
+import React, { useState } from "react";
+import { MuiTelInput } from "mui-tel-input";
+
 import {
-    Modal,
-    Box,
-    Typography,
-    IconButton,
     Button,
     TextField,
+    Alert,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogContentText,
+    DialogActions,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
+    Snackbar,
 } from "@mui/material";
-import {CloseIcon, AddIcon , DeleteIcon , } from "@mui/icons-material";
 
-import "../modal/modal.css";
 
-const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "background.paper",
-    border: "2px solid #000",
-    boxShadow: 24,
-    p: 4,
-};
 
 const ReferralModal = ({ open, onClose }) => {
+    const [value, setPhone] = useState("");
+    const [snackbarOpen, setSnackbarOpen] = useState(false);
+
+
+    const handleChange = (newValue) => {
+        setPhone(newValue);
+    };
+
+    const helper = () => {
+        onClose();
+        setPhone("");
+    };
+
     return (
         <>
-            <Modal open={open} onClose={onClose}>
-                <Box sx={style}>
-                    <Typography
-                        id="modal-modal-title"
-                        variant="h6"
-                        component="h2"
-                    >
-                        Text in a modal
-                    </Typography>
-                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                        Dui mollis, est non commodo luctus, nisi erat porttitor
-                        ligula.
-                    </Typography>
-                </Box>
-            </Modal>
+            <Dialog
+                open={open}
+                onClose={helper}
+                slotProps={{
+                    paper: {
+                        component: "form",
+                        onSubmit: (event) => {
+                            event.preventDefault();
+                            const formData = new FormData(event.currentTarget);
+                            const formJson = Object.fromEntries(
+                                formData.entries()
+                            );
+                            // const email = formJson;
+                            console.log(formJson);
+                            setPhone("");
+                            setSnackbarOpen(true);
+                            onClose();
+                        },
+                    },
+                }}
+            >
+                <DialogTitle>Refer a Friend</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Fill the details below to refer a friend
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        required
+                        margin="dense"
+                        id="referrer_email"
+                        name="referrer_email"
+                        label="Your email address"
+                        type="email"
+                        fullWidth
+                        variant="outlined"
+                    />
+                    <TextField
+                        autoFocus
+                        required
+                        margin="dense"
+                        id="referee_email"
+                        name="referee_email"
+                        label="Email address of your friend"
+                        type="email"
+                        fullWidth
+                        variant="outlined"
+                    />
+                    <TextField
+                        autoFocus
+                        required
+                        margin="dense"
+                        id="referee_name"
+                        name="referee_name"
+                        label="Name of your friend"
+                        type="text"
+                        fullWidth
+                        variant="outlined"
+                    />
+                    <MuiTelInput
+                        autoFocus
+                        required
+                        margin="dense"
+                        id="referee_number"
+                        name="referee_number"
+                        label="Number of your friend"
+                        fullWidth
+                        value={value}
+                        onChange={handleChange}
+                    />
+                    <FormControl fullWidth margin="dense" required>
+                        <InputLabel id="menu">Courses</InputLabel>
+                        <Select label="courses" name="courses">
+                            <MenuItem value={"mongodb"}>Mongodb</MenuItem>
+                            <MenuItem value={"express"}>Express</MenuItem>
+                            <MenuItem value={"node"}>Node</MenuItem>
+                            <MenuItem value={"react"}>React</MenuItem>
+                        </Select>
+                    </FormControl>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={helper}>Cancel</Button>
+                    <Button type="submit">Refer</Button>
+                </DialogActions>
+            </Dialog>
+            <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={4000} // Hide after 4 seconds
+                onClose={() => setSnackbarOpen(false)}
+                anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+            >
+                <Alert
+                    onClose={() => setSnackbarOpen(false)}
+                    severity="success"
+                >
+                    Referral Successful, we will send you an email.
+                </Alert>
+            </Snackbar>
         </>
     );
 };
--- src/pages/refer/refer.js
+++ src/pages/refer/refer.js
@@ -1,13 +1,25 @@
-//
+import React, {useState} from 'react';
 import Layout from "../../components/layout/layout";
 import { Button, Container, Typography, Box } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import scholar from "../../assests/images/scholar.svg";
-const refer = () => {
+import ReferalModal from '../../components/modal/modal'
+
+const Refer = () => {
+    const [openModal, setOpenModal] = useState(false);
+
+    const openModalHandler = () => {
+      setOpenModal(true);
+    };
+  
+    const closeModalHandler = () => {
+      setOpenModal(false);
+    };
+
     return (
         <Layout>
-            <Container sx={{marginTop:2}}>
-                <Grid container spacing={4} alignItems="center">
+            <Container className="grid-container" sx={{ my: 2, bgcolor:"#d9f3fa", p:2 }} >
+                <Grid container spacing={4} alignItems="center" >
                     <Grid size={{ xs: 12, md: 5 }}>
                         <Typography variant="h3" gutterBottom>
                             Refer & Earn Rewards
@@ -25,30 +37,28 @@ const refer = () => {
                             variant="contained"
                             size="large"
                             color="secondary"
+                            onClick={openModalHandler}
                             sx={{ mt: 2, px: 4, py: 1.5, fontWeight: "bold" }}
                         >
                             Refer Now
                         </Button>
                     </Grid>
                     <Grid size={{ xs: 12, md: 7 }}>
-                        {/* <HeroImage
-                            src="/images/refer-and-earn.svg"
-                            alt="Refer and Earn Illustration"
-                        /> */}
-                            <Box
-                                component="img"
-                                sx={{
-                                    height: "90%",
-                                    maxWidth: '100%'
-                                }}
-                                src={scholar}
-                                alt="logo image"
-                            />
+                        <Box
+                            component="img"
+                            sx={{
+                                height: "90%",
+                                maxWidth: "100%",
+                            }}
+                            src={scholar}
+                            alt="logo image"
+                        />
                     </Grid>
                 </Grid>
+                <ReferalModal open={openModal} onClose={closeModalHandler}/>
             </Container>
         </Layout>
     );
 };
 
-export default refer;
+export default Refer;
